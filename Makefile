314d3cb842c3e2fb02a3e7f56689a88c0fe9a9c3
Vasily i. Redkin <vir@ctm.ru>
2014-10-20 15:57:26 +0400

    Advanced grep with channels processing implemented.

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
+CFLAGS?=-O2
 
 .PHONY: clean
 
 .cpp.o: $<
-	g++ -Wall -O2 -I`yate-config --includes` $(DEBUG) -Wno-overloaded-virtual -fno-exceptions -DHAVE_GCC_FORMAT_CHECK -DHAVE_BLOCK_RETURN -I/usr/include/yate -c -o $@ $^
+	g++ -Wall $(CFLAGS) -I`yate-config --includes` $(DEBUG) -Wno-overloaded-virtual -fno-exceptions -DHAVE_GCC_FORMAT_CHECK -DHAVE_BLOCK_RETURN -I/usr/include/yate -c -o $@ $^
 
 all: yategrep
 
@@ -15,13 +16,13 @@ clean:
 	rm -f $(patsubst %.cpp,%.o,$(wildcard *.cpp)) yategrep
 
 debug:
-	$(MAKE) all DEBUG=-g3 MODSTRIP=
+	$(MAKE) all DEBUG=-g3 MODSTRIP= CFLAGS=
 
 ddebug:
-	$(MAKE) all DEBUG='-g3 -DDEBUG' MODSTRIP=
+	$(MAKE) all DEBUG='-g3 -DDEBUG' MODSTRIP= CFLAGS=
 
 xdebug:
-	$(MAKE) all DEBUG='-g3 -DXDEBUG' MODSTRIP=
+	$(MAKE) all DEBUG='-g3 -DXDEBUG' MODSTRIP= CFLAGS=
 
 ndebug:
 	$(MAKE) all DEBUG='-g0 -DNDEBUG'
--- yategrep.cpp
+++ yategrep.cpp
@@ -32,21 +32,40 @@ class Query
 public:
 	Query()
 		: m_params("QueryParams")
-		, m_modified(false)
 	{
 	}
 	TelEngine::NamedList& params()
 		{ return m_params; }
-	bool matches(const Entry& e, bool partial = false);
-	bool modified() const
-		{ return m_modified; }
+	const TelEngine::NamedList& params() const
+		{ return m_params; }
+	bool matches(const Entry& e, bool partial = false) const;
+	bool update(const Entry& e, bool reset); /**< Updates query with new channels and addresses from log entry. @return true if query was really modified */
+	void dump(TelEngine::Stream& out)
+	{
+		out.writeData("Params:\n ");
+		TelEngine::String d;
+		m_params.dump(d, " ", '\'', true);
+		out.writeData(d);
+
+		d = "\nChannels(";
+		d << m_channels.count() << "/" << m_channels.length() << "):\n";
+		for(TelEngine::ObjList* p = m_channels.skipNull(); p; p = p->skipNext()) {
+			d << " " << p->get()->toString();
+		}
+		out.writeData(d);
+		out.writeData("\nAddresses:\n");
+		for(TelEngine::ObjList* p = m_addrs.skipNull(); p; p = p->skipNext()) {
+			out.writeData(" ");
+			out.writeData(p->toString());
+		}
+		out.writeData("\n");
+	}
 private:
 	TelEngine::NamedList m_params;
 	TelEngine::ObjList m_channels;
 	unsigned int m_newChannels;
 	TelEngine::ObjList m_addrs;
 	unsigned int m_newAddrs;
-	bool m_modified;
 };
 
 class Parser
@@ -143,41 +162,109 @@ public:
 	}
 	void run(TelEngine::Stream& in, TelEngine::Stream& out);
 private:
+	void output(const Entry& e, TelEngine::Stream& out);
 	Query& m_query;
 	RingBuf<Entry> m_buf;
 };
 
-bool Query::matches(const Entry& e, bool partial /* = false */)
+static bool isChannelParam(const TelEngine::String& name)
 {
-	if(! partial)
-		m_modified = false;
-	if(/*e.type() != Entry::MESSAGE || */! e.count())
-		return false;
-	bool match = false;
-	unsigned qn = params().length();
-//fprintf(stderr, "Query: %u params\n", qn);
+	using namespace TelEngine;
+//fprintf(stderr, "isChannelParam(%s)\n", name.c_str());
+	if(name == YSTRING("id"))
+		return true;
+	if(name == YSTRING("targetid"))
+		return true;
+	if(name == YSTRING("peerid"))
+		return true;
+	// XXX there are lots of fork-generated names XXX
+	return false;
+}
+
+static bool fullMatch(const TelEngine::NamedList& key, const TelEngine::NamedList& entry)
+{
+	unsigned int n = entry.length();
+	unsigned int qn = key.length();
+#if 0
+TelEngine::String d1, d2;
+key.dump(d1, " ", '\'', true);
+entry.dump(d2, " ", '\'', true);
+fprintf(stderr, "Checkong entry %s against key %s\n", d2.c_str(), d1.c_str());
+#endif
 	for(unsigned int qi = 0; qi < qn; ++qi) {
-		TelEngine::NamedString* q = params().getParam(qi);
+		TelEngine::NamedString* q = key.getParam(qi);
 		if(! q)
 			continue;
+		bool found = false;
+		for(unsigned int i = 0; i < n; ++i) {
+			TelEngine::NamedString* s = entry.getParam(i);
+			if(! s)
+				continue;
+			if(s->name() == q->name()) {
+				found = true;
+				if(*s != *q)
+					return false; /* AND logic, fail on first non-equal param */
+				else
+					break;
+			}
+		}
+		if(! found)
+			return false; /* all requested parameters should be here */
+	}
+	return true;
+}
+
+bool Query::matches(const Entry& e, bool partial /* = false */) const
+{
+	if(!partial) { /* Full match */
+		if(e.type() == Entry::MESSAGE && fullMatch(params(), e))
+			return true;
+	}
+
+	for(TelEngine::ObjList* chans = m_channels + (partial ? m_newChannels : 0); chans; chans = chans->skipNext()) { // check channel names
+		TelEngine::GenObject* o = chans->get();
+		if(! o)
+			continue;
+		TelEngine::String chan = o->toString();
 		unsigned int n = e.length();
-//fprintf(stderr, "Entry: %u params (checking query param %u\n", n, qi);
 		for(unsigned int i = 0; i < n; ++i) {
 			TelEngine::NamedString* s = e.getParam(i);
 			if(! s)
 				continue;
-			if(s->name() == q->name()) {
-				match = true;
-				if(*s != *q)
-					return false; /* AND logic, fail on first non-equal param */
-			}
+			if(! isChannelParam(s->name()))
+				continue;
+			if(*s == chan)
+				return true;
 		}
 	}
-	if(! match)
-		return false;
-	/* positive match! */
+	// TODO: check addresses
+	return false;
+}
+
+/* Updates query with new channels and addresses from log entry. @return true if query was really modified */
+bool Query::update(const Entry& e, bool reset)
+{
+	if(reset) {
+		m_newChannels = m_channels.count();
+		m_newAddrs = m_addrs.count();
+	}
+fprintf(stderr, "Query::update called\n");
+	bool modified = false;
+	unsigned int n = e.length();
+	for(unsigned int i = 0; i < n; ++i) {
+		TelEngine::NamedString* s = e.getParam(i);
+		if(! s)
+			continue;
+		if(! isChannelParam(s->name()))
+			continue;
+		if(m_channels.find(*s))
+			continue;
+fprintf(stderr, "passwd 3ed(%s=%s)\n", s->name().c_str(), s->c_str());
+		m_channels.append(new TelEngine::String(*s), false);
+		modified = true;
+	}
 	/* XXX TODO: check for new channels, append, set modified flag */
-	return true;
+	return modified;
 }
 
 TelEngine::String Parser::getLine(int eol /* = '\n'*/)
@@ -301,37 +388,56 @@ void Grep::run(TelEngine::Stream& in, TelEngine::Stream& out)
 	while(( e = parser.get() )) {
 		if(m_query.matches(*e)) {
 			e->mark();
-			if(m_query.modified()) {
-				for(size_t index = 1; index < m_buf.count(); ++index) {
-					if(m_query.matches(*m_buf.at(index), true))
-						m_buf.at(index)->mark();
-				}
+#if 1 /* DEEP SEARCH */
+			if(m_query.update(*e, true)) {
+				int count;
+				do {
+					count = 0;
+					for(size_t index = 0; index < m_buf.count() - 1; ++index) {
+						Entry* e = m_buf.at(index);
+						if(!e)
+							continue;
+						if(m_query.matches(*e, true)) {
+							m_buf.at(index)->mark();
+							if(m_query.update(*e, false))
+								++count;
+						}
+					}
+				} while(count);
 			}
+#endif
 		}
 		m_buf.push(e);
 
 		if(m_buf.avail() < 1) {
 			e = m_buf.pop();
-			if(e->marked())
-				out.writeData("MARK>>> ");
-			out.writeData(*e);
+			output(*e, out);
 			delete e;
 		}
 	}
 	while(! m_buf.empty()) {
 		e = m_buf.pop();
-		if(e->marked())
-			out.writeData("MARK>>> ");
-		out.writeData(*e);
+		output(*e, out);
 		delete e;
 	}
 #endif
 }
 
+void Grep::output(const Entry& e, TelEngine::Stream& out)
+{
+	if(e.marked())
+		out.writeData("MARK>>> ");
+#if 0
+	out.writeData(TelEngine::String((int)e.type()));
+	out.writeData(") ");
+#endif
+	out.writeData(e);
+}
+
 int main(int argc, const char* argv[])
 {
 	Query query;
-	query.params().setParam("billid", "1413175501-2747");
+	query.params().setParam("billid", "1413261902-39");
 //	query.params().setParam("nonce", "993d17cc6f3b5f8a35c88b3936d3e0d7.1413253502");
 //	query.params().setParam("nodename", "unicorn");
 	Grep grep(query);
@@ -344,5 +450,6 @@ int main(int argc, const char* argv[])
 
 	TelEngine::File output(1);
 	grep.run(input, output);
+	query.dump(output);
 	return 0;
 }
